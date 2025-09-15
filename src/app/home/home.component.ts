ce211b8743a285cffa46e9761af62ed74d8164e5
Niroop-P-S <psniroop19@gmail.com>
2025-09-16 03:37:38 +0530

    fix: remove unused LocationsComponent from home component imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
 import { AboutTabComponent } from '../about-tab/about-tab.component';
-import { LocationsComponent } from '../locations/locations.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutTabComponent, LocationsComponent],
+  imports: [HeroComponent, AboutTabComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
